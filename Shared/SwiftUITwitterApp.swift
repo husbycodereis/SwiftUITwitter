3e5f6b9afdefd5020c56786a37a89081a9e70230
Ali Rıza Reisoğlu <alirizareisoglu@gmail.com>
2022-08-05 10:00:18 +0300

    Initial Commit

---
+++ Shared/SwiftUITwitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITwitterApp.swift
+//  Shared
+//
+//  Created by Ali Riza Reisoglu on 5.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITwitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
